14359f81825b97d055fdb31aaad44b1b40819e24
Sam_Schmidt <schmidts@grinnell.edu>
2024-10-01 09:05:51 -0500

    Add Tally class and corresponding experiments

--- src/test/java/edu/grinnell/csc207/CounterTests.java
+++ src/test/java/edu/grinnell/csc207/CounterTests.java
@@ -3,6 +3,7 @@ package edu.grinnell.csc207;
 import edu.grinnell.csc207.util.BasicCounter;
 import edu.grinnell.csc207.util.Counter;
 // import edu.grinnell.csc207.util.BasicCounter;
+import edu.grinnell.csc207.util.Tally;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -15,41 +16,17 @@ import org.junit.jupiter.api.Test;
  * Tests of our counter class.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
- * @author Your Name Here
+ * @author Sam Schmidt
+ * @author Paden Houck
  */
 public class CounterTests {
-    public static void main(String[] args) throws Exception {
-    // Set up output
-    PrintWriter pen = new PrintWriter(System.out, true);
-
-    // Set up some counters
-    Counter alpha = new BasicCounter(0);
-    Counter beta = new BasicCounter(123);
-    Counter gamma = new BasicCounter(-5);
-
-    // Print original values
-    pen.println("Original alpha = " + alpha);
-    pen.println("Original beta = " + beta);
-    pen.println("Original gamma = " + gamma);
-  
-    // Print incremented values
-    alpha.increment();
-    beta.increment();
-    gamma.increment();
-    pen.println("Updated alpha = " + alpha);
-    pen.println("Updated beta = " + beta);
-    pen.println("Updated gamma = " + gamma);
-
-    // And we're done
-    pen.close();
-  } // main(String[])
+    
     /**
      * Some basic tests of our BasicCounter objects.
      */
     @Test
     public void test1() throws Exception {
-        Counter alpha = new BasicCounter(0);
+        Counter alpha = new Tally();
         Counter beta = new BasicCounter(123);
         Counter gamma = new BasicCounter(-5);
         assertEquals(0, alpha.get(), "original alpha");
